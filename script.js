98f8b57ac3000c258d0e7b9a5f442a1b951723e3
sandeep0620 <harisunny20@gmail.com>
2024-05-23 22:57:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = e.target.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetSection.offsetTop - 50,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
